9e38ada268df229cc3e5699ef992536fb042036f
nohturfft <nohturfft@gmail.com>
2021-03-02 16:24:44 +0000

    First commit

---
+++ packages.R
@@ -0,0 +1,42 @@
+if (grepl("Linux stats3", system("uname -a", intern=T))) {
+  lib.dir <- "/homes/homedirs18/sghms/bms/shares/bdib/BDiB_2020-21/R/packages_R_3.5.3"
+} else {
+  lib.dir <- NULL
+}
+
+if (is.null(lib.dir)) {
+  deps <- c("Imports", "Depends")
+  
+  if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager", dependencies=deps)
+  }
+  
+  if (!requireNamespace("GEOquery", quietly = TRUE)) {
+    BiocManager::install("GEOquery", dependencies=deps)
+  }
+  
+  if (!requireNamespace("magrittr", quietly = TRUE)) {
+    install.packages("magrittr", dependencies=deps)
+  }
+  
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    install.packages("dplyr", dependencies=deps)
+  }
+  
+  if (!requireNamespace("details", quietly = TRUE)) {
+    install.packages("details", dependencies=deps)
+  }
+  
+  if (!requireNamespace("knitr", quietly = TRUE)) {
+    install.packages("knitr", dependencies=deps)
+  }
+  
+  rm(deps)
+}
+
+library(magrittr)
+
+
+
+
+  
